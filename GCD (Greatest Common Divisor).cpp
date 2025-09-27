2a636c91d61061b65f975a7330f376679888cb48
Md. Sarawer Bhuiyan <130220612+sarawer@users.noreply.github.com>
2025-09-27 19:49:03 +0600

    Add files via upload

---
+++ GCD (Greatest Common Divisor).cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+
+//Simple Method
+int gcd1(int a,int b){
+	int m=min(a,b);
+	while(m>1){
+		if(a%m==0 && b%m==0){
+				break;
+		}else m--;
+	}
+	return m;
+}
+
+//using Euclid's Algorithm
+int gcd2(int a,int b){
+	if(b==0)return a;
+	return gcd2(b,a%b);
+}
+
+int32_t main(){
+	int a,b;
+	cin>>a>>b;
+	cout<<gcd1(a,b)<<endl;
+	cout<<gcd2(a,b)<<endl;	
+}
