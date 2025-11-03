38def579b5ab4f3532f910e43fa7faa148736d79
Md. Sarawer Bhuiyan <130220612+sarawer@users.noreply.github.com>
2025-11-03 12:20:42 +0600

    Refactor prefix sum to 1-based indexing with sum query
    Updated prefix sum implementation to support 1-based indexing and included a sum query feature.

--- prefix sum.cpp
+++ prefix sum.cpp
@@ -4,14 +4,18 @@ using namespace std;
 int main() {
     int n;
     cin>>n;
-    vector<int>v(n);
+    vector<int>v(n); // 0-based index
     for(int i=0;i<n;i++)cin>>v[i];
 
-    vector<int>prefixsum(n);
-    prefixsum[0]=v[0];
-    for(int i=1;i<n;i++){
-        prefixsum[i]=prefixsum[i-1]+v[i];
+    vector<int>pref(n+1);   // 1-based index
+    for(int i=1;i<=n;i++){
+        pref[i]=pref[i-1]+v[i-1];
     }
 
-    for(auto u:prefixsum)cout<<u<<' ';
+    for(auto u:pref)cout<<u<<' ';
+
+    //find sum of intiger from l to r
+    int l=3,r=5; // 1-based index
+    int sum = pref[r] - pref[l-1];
+    cout<<endl<<sum;
 }
